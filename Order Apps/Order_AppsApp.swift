09e7a06d0ae1805e3e99b39ef680e391072c46f2
William Santoso <wsantoso@otospector.co.id>
2021-08-21 20:04:42 +0700

    Initial Commit

---
+++ Order Apps/Order_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Order_AppsApp.swift
+//  Order Apps
+//
+//  Created by William Santoso on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Order_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
